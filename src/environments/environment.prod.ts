01d53bf1115937f0f217e83ac6deac676eb6205d
Ravi Prakash <raviprakash438@gmail.com>
2020-08-15 12:10:21 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  name:"prod",
+  apiUrl:"http://prodUrl.com"
+
 };
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  name:"qa",
+  apiUrl:"http://qaUrl.com"
+
+};
